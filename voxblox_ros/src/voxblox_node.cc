713d8f80a762be14ec553496347266bcf614ba58
Helen Oleynikova <helenoleynikova@gmail.com>
2016-04-15 14:39:28 +0200

    Added pointcloud output.

--- voxblox_ros/src/voxblox_node.cc
+++ voxblox_ros/src/voxblox_node.cc
@@ -1,8 +1,12 @@
 #include <minkindr_conversions/kindr_msg.h>
 #include <minkindr_conversions/kindr_tf.h>
+#include <pcl/conversions.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
 #include <ros/ros.h>
-#include <tf/transform_listener.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_listener.h>
+#include <visualization_msgs/MarkerArray.h>
 
 #include "voxblox/core/map.h"
 
@@ -15,6 +19,18 @@ class VoxbloxNode {
  public:
   VoxbloxNode(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private)
       : nh_(nh), nh_private_(nh_private), world_frame_("world") {
+    // Advertise topics.
+    sdf_marker_pub_ = nh_private_.advertise<visualization_msgs::MarkerArray>(
+        "sdf_markers", 1, true);
+    sdf_pointcloud_pub_ =
+        nh_private_.advertise<visualization_msgs::MarkerArray>("sdf_pointcloud",
+                                                               1, true);
+
+    pointcloud_sub_ = nh_.subscribe("pointcloud", 40,
+                                    &VoxbloxNode::insertPointcloudWithTf, this);
+
+    // 16 vps at 0.2 resolution. TODO(helenol): load these from params for
+    // faster prototyping...
     tsdf_map_.reset(new TsdfMap(16, 0.2));
   }
 
@@ -44,6 +60,7 @@ class VoxbloxNode {
 
   // Publish markers for visualization.
   ros::Publisher sdf_marker_pub_;
+  ros::Publisher sdf_pointcloud_pub_;
 
   std::shared_ptr<TsdfMap> tsdf_map_;
 };
@@ -63,10 +80,53 @@ void VoxbloxNode::insertPointcloudWithTf(
   // in sensor frame.
 }
 
+void VoxbloxNode::publishMarkers() {
+  CHECK(tsdf_map_) << "TSDF map not allocated.";
+
+  // Create a pointcloud with distance = intensity.
+  pcl::PointCloud<pcl::PointXYZI> pointcloud;
+
+  // Iterate over all voxels to create a pointcloud.
+  // TODO(helenol): move this to general IO, replace ply writer with writing
+  // this out.
+  size_t num_blocks = tsdf_map_->getNumberOfAllocatedBlocks();
+  // This function is block-specific:
+  size_t num_voxels_per_block = tsdf_map_->getVoxelsPerBlock();
+
+  pointcloud.reserve(num_blocks * num_voxels_per_block);
+
+  BlockIndexList blocks;
+  tsdf_map_->getAllAllocatedBlocks(&blocks);
+
+  // Iterate over all blocks.
+  const float max_distance = 0.5;
+  for (const BlockIndex& index : blocks) {
+    // Iterate over all voxels in said blocks.
+    const TsdfBlock& block = tsdf_map_->getBlockByIndex(index);
+    for (size_t i = 0; i < num_voxels_per_block; ++i) {
+      float distance = block.getTsdfVoxelByLinearIndex(i).distance;
+
+      // Get back the original coordinate of this voxel.
+      Coordinates coord = block.getCoordinatesOfTsdfVoxelByLinearIndex(i);
+
+      pcl::PointXYZI point;
+      point.x = coord.x();
+      point.y = coord.y();
+      point.z = coord.z();
+      point.intensity = distance;
+      pointcloud.push_back(point);
+    }
+  }
+
+  pointcloud.header.frame_id = world_frame_;
+  sdf_pointcloud_pub_.publish(pointcloud);
+}
+
 // Stolen from octomap_manager
-bool VoxbloxNode::lookupTransform(
-    const std::string& from_frame, const std::string& to_frame,
-    const ros::Time& timestamp, Transformation* transform) {
+bool VoxbloxNode::lookupTransform(const std::string& from_frame,
+                                  const std::string& to_frame,
+                                  const ros::Time& timestamp,
+                                  Transformation* transform) {
   tf::StampedTransform tf_transform;
 
   ros::Time time_to_lookup = timestamp;
