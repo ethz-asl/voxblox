a44acf4653f5341f76f0d379b87f6dd4c87905a7
Helen Oleynikova <helenoleynikova@gmail.com>
2016-05-13 12:47:08 +0200

    First draft of meshing, mostly from open_chisel.

--- voxblox/src/voxblox.cc
+++ voxblox/src/voxblox.cc
@@ -1,5 +1,6 @@
 #include "voxblox/core/tsdf_map.h"
 #include "voxblox/integrator/tsdf_integrator.h"
+#include "voxblox/mesh/mesh_integrator.h"
 #include "voxblox/io/sdf_ply.h"
 
 #include <gflags/gflags.h>
