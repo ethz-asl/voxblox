87277fb5e45f37495e019e0444ac20785d79ee5b
Helen Oleynikova <helenoleynikova@gmail.com>
2016-05-24 09:36:21 +0200

    Review comments.

--- voxblox/include/voxblox/integrator/tsdf_integrator.h
+++ voxblox/include/voxblox/integrator/tsdf_integrator.h
@@ -20,9 +20,8 @@ class TsdfIntegrator {
     float default_truncation_distance = 0.1;
     float max_weight = 100.0;
     bool voxel_carving_enabled = true;
-    // Units below: meters.
-    FloatingPoint min_ray_length = 1e-3;
-    FloatingPoint max_ray_length = 5.0;
+    FloatingPoint min_ray_length_m = 1e-3;
+    FloatingPoint max_ray_length_m = 5.0;
   };
 
   TsdfIntegrator(Layer<TsdfVoxel>* layer, const Config& config)
@@ -84,9 +83,9 @@ class TsdfIntegrator {
       const Color& color = colors[pt_idx];
 
       FloatingPoint ray_distance = (point_G - origin).norm();
-      if (ray_distance < config_.min_ray_length) {
+      if (ray_distance < config_.min_ray_length_m) {
         continue;
-      } else if (ray_distance > config_.max_ray_length) {
+      } else if (ray_distance > config_.max_ray_length_m) {
         // TODO(helenol): clear until max ray length instead.
         continue;
       }
@@ -165,9 +164,9 @@ class TsdfIntegrator {
       const Point point_G = T_G_C * point_C;
 
       FloatingPoint ray_distance = (point_G - origin).norm();
-      if (ray_distance < config_.min_ray_length) {
+      if (ray_distance < config_.min_ray_length_m) {
         continue;
-      } else if (ray_distance > config_.max_ray_length) {
+      } else if (ray_distance > config_.max_ray_length_m) {
         // TODO(helenol): clear until max ray length instead.
         continue;
       }
@@ -178,7 +177,7 @@ class TsdfIntegrator {
       voxel_map[voxel_index].push_back(pt_idx);
     }
 
-    LOG(5) << "Went from " << points_C.size() << " points to "
+    VLOG(5) << "Went from " << points_C.size() << " points to "
            << voxel_map.size() << " raycasts.";
 
     FloatingPoint truncation_distance = config_.default_truncation_distance;
