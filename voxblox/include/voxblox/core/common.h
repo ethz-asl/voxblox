41e569455c2ed702073bc7ad14734305a191fc34
Zachary Taylor <ztaylor@ethz.ch>
2016-06-02 15:47:03 +0200

    starting interpolation

--- voxblox/include/voxblox/core/common.h
+++ voxblox/include/voxblox/core/common.h
@@ -16,7 +16,7 @@ typedef double FloatingPoint;
 typedef Eigen::Matrix<FloatingPoint, 3, 1> Point;
 typedef Eigen::Matrix<FloatingPoint, 3, 1> Ray;
 
-typedef Eigen::Vector3i AnyIndex;
+typedef Eigen::Array<int, 3, 1> AnyIndex;
 typedef AnyIndex VoxelIndex;
 typedef AnyIndex BlockIndex;
 
@@ -33,6 +33,16 @@ typedef std::vector<Triangle, Eigen::aligned_allocator<Triangle> >
 
 // Transformation type for defining sensor orientation.
 typedef kindr::minimal::QuatTransformation Transformation;
+typedef kindr::minimal::RotationQuaternion Rotation;
+
+// For alignment of layers / point clouds
+typedef Eigen::Matrix<FloatingPoint, 3, Eigen::Dynamic> PointsMatrix;
+typedef Eigen::Matrix<FloatingPoint, 3, 3> Matrix3;
+
+// Interpolation structure
+typedef Eigen::Matrix<FloatingPoint, 8 ,8> InterpTable;
+typedef Eigen::Matrix<FloatingPoint, 1 ,8> InterpVector;
+typedef Eigen::Array<int, 3, 8> InterpIndexes; //type must allow negatives
 
 struct Color {
   Color() : r(0), g(0), b(0), a(0) {}
---
+++ voxblox/include/voxblox/interpolator/interpolator.h
@@ -0,0 +1,38 @@
+#ifndef VOXBLOX_INTERPOLATOR_H_
+#define VOXBLOX_INTERPOLATOR_H_
+
+namespace voxblox {
+
+class Interpolator {
+ public:
+  Interpolator(const Layer<TsdfVoxel>& tsdf_layer);
+
+  bool getGradient(const Point& pos, Point* grad,
+                   const bool interpolate = false) const;
+
+  bool getDistance(const Point& pos, FloatingPoint* distance,
+                   bool interpolate = false) const;
+
+ private:
+  bool setIndexes(const Point& pos, BlockIndex* block_index,
+                  InterpIndexes* voxel_indexes) const;
+
+  void getWeightsVector(const Point& voxel_pos, const Point& pos,
+                        InterpVector* weights_vector) const;
+
+  bool getDistancesAndWeights(const BlockIndex& block_index,
+                              const InterpIndexes& voxel_indexes,
+                              const Point& pos, InterpVector* distances,
+                              InterpVector* weights_vector) const;
+
+  bool getInterpDistance(const Point& pos, FloatingPoint* distance) const;
+
+  bool getNearestDistance(const Point& pos, FloatingPoint* distance) const;
+
+  const Layer<TsdfVoxel>& tsdf_layer_;
+};
+}
+
+#include "voxblox/interpolator/interpolator_inl.h"
+
+#endif  // VOXBLOX_INTERPOLATOR_H_
