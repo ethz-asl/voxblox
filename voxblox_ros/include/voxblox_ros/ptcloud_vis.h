b36843220d7b3ee76360ce762a29a49dce36a859
Helen Oleynikova <helenoleynikova@gmail.com>
2016-06-29 13:42:51 +0200

    Breaking out visualization into separate functions to mix-and-match as necessary.

--- voxblox_ros/include/voxblox_ros/ptcloud_vis.h
+++ voxblox_ros/include/voxblox_ros/ptcloud_vis.h
@@ -1,5 +1,5 @@
-#ifndef VOXBLOX_ROS_MESH_VIS_H_
-#define VOXBLOX_ROS_MESH_VIS_H_
+#ifndef VOXBLOX_ROS_PTCLOUD_VIS_H_
+#define VOXBLOX_ROS_PTCLOUD_VIS_H_
 
 #include <algorithm>
 #include <visualization_msgs/Marker.h>
@@ -18,14 +18,134 @@
 
 namespace voxblox {
 
+// Shortcut the placeholders namespace, since otherwise it chooses the boost
+// placeholders which are in the global namespace (thanks boost!).
+namespace ph = std::placeholders;
+
+/// This is a fancy alias to be able to template functions.
 template <typename VoxelType>
-typedef std::function<bool(const VoxelType& voxel, Color* color)> // NOLINT
-      ShouldVisualizeVoxelFunctionType;
+using ShouldVisualizeVoxelColorFunctionType = std::function<bool(
+    const VoxelType& voxel, const Point& coord, Color* color)>;
 
+template <typename VoxelType>
+using ShouldVisualizeVoxelIntensityFunctionType = std::function<bool(
+    const VoxelType& voxel, const Point& coord, double* intensity)>;
 
+// Template function to visualize a colored pointcloud.
+template <typename VoxelType>
+void createColorPointcloudFromLayer(
+    const Layer<VoxelType>& layer,
+    const ShouldVisualizeVoxelColorFunctionType<VoxelType>& vis_function,
+    pcl::PointCloud<pcl::PointXYZRGB>* pointcloud) {
+  CHECK_NOTNULL(pointcloud);
+  pointcloud->clear();
+  BlockIndexList blocks;
+  layer.getAllAllocatedBlocks(&blocks);
 
+  // Cache layer settings.
+  size_t vps = layer.voxels_per_side();
+  size_t num_voxels_per_block = vps * vps * vps;
 
+  // Temp variables.
+  Color color;
+  // Iterate over all blocks.
+  for (const BlockIndex& index : blocks) {
+    // Iterate over all voxels in said blocks.
+    const Block<VoxelType>& block = layer.getBlockByIndex(index);
+
+    for (size_t linear_index = 0; linear_index < num_voxels_per_block;
+         ++linear_index) {
+      Point coord = block.computeCoordinatesFromLinearIndex(linear_index);
+      if (vis_function(block.getVoxelByLinearIndex(linear_index), coord,
+                       &color)) {
+        pcl::PointXYZRGB point;
+        point.x = coord.x();
+        point.y = coord.y();
+        point.z = coord.z();
+        point.r = color.r;
+        point.g = color.g;
+        point.b = color.b;
+        pointcloud->push_back(point);
+      }
+    }
+  }
+}
+
+// Template function to visualize an intensity pointcloud.
+template <typename VoxelType>
+void createColorPointcloudFromLayer(
+    const Layer<VoxelType>& layer,
+    const ShouldVisualizeVoxelIntensityFunctionType<VoxelType>& vis_function,
+    pcl::PointCloud<pcl::PointXYZI>* pointcloud) {
+  CHECK_NOTNULL(pointcloud);
+  pointcloud->clear();
+  BlockIndexList blocks;
+  layer.getAllAllocatedBlocks(&blocks);
+
+  // Cache layer settings.
+  size_t vps = layer.voxels_per_side();
+  size_t num_voxels_per_block = vps * vps * vps;
+
+  // Temp variables.
+  double intensity = 0.0;
+  // Iterate over all blocks.
+  for (const BlockIndex& index : blocks) {
+    // Iterate over all voxels in said blocks.
+    const Block<VoxelType>& block = layer.getBlockByIndex(index);
+
+    for (size_t linear_index = 0; linear_index < num_voxels_per_block;
+         ++linear_index) {
+      Point coord = block.computeCoordinatesFromLinearIndex(linear_index);
+      if (vis_function(block.getVoxelByLinearIndex(linear_index), coord,
+                       &intensity)) {
+        pcl::PointXYZI point;
+        point.x = coord.x();
+        point.y = coord.y();
+        point.z = coord.z();
+        point.intensity = intensity;
+        pointcloud->push_back(point);
+      }
+    }
+  }
+}
+
+// Short-hand functions for visualizing different types of voxels.
+bool visualizeNearSurfaceTsdfVoxels(const TsdfVoxel& voxel, const Point& coord,
+                                    double surface_distance, Color* color) {
+  if (voxel.weight > 0 && std::abs(voxel.distance) < surface_distance) {
+    *color = voxel.color;
+    return true;
+  }
+  return false;
+}
+
+bool visualizeDistanceIntensityTsdfVoxels(const TsdfVoxel& voxel,
+                                          const Point& coord,
+                                          double* intensity) {
+  if (voxel.weight > 1e-3) {
+    *intensity = voxel.distance;
+    return true;
+  }
+  return false;
+}
+
+// All functions that can be used directly for TSDF voxels.
+void createSurfacePointcloudFromTsdfLayer(
+    const Layer<TsdfVoxel>& layer, double surface_distance,
+    pcl::PointCloud<pcl::PointXYZRGB>* pointcloud) {
+  createColorPointcloudFromLayer<TsdfVoxel>(
+      layer, std::bind(&visualizeNearSurfaceTsdfVoxels, ph::_1, ph::_2,
+                       surface_distance, ph::_3),
+      pointcloud);
+}
+
+void createDistancePointcloudFromTsdfLayer(
+    const Layer<TsdfVoxel>& layer,
+    pcl::PointCloud<pcl::PointXYZI>* pointcloud) {
+  createColorPointcloudFromLayer<TsdfVoxel>(
+      layer, &visualizeDistanceIntensityTsdfVoxels, pointcloud);
+}
 
 }  // namespace voxblox
 
-#endif  // VOXBLOX_ROS_MESH_VIS_H_
+#endif  // VOXBLOX_ROS_PTCLOUD_VIS_H_
