8a3e8e0fb5e2445199bc5085adc8e7de5cd25ada
Helen Oleynikova <helenoleynikova@gmail.com>
2016-08-31 15:24:58 +0200

    Starting skeleton of eval script.

---
+++ voxblox_ros/src/voxblox_eval.cc
@@ -0,0 +1,178 @@
+#include <deque>
+#include <minkindr_conversions/kindr_msg.h>
+#include <minkindr_conversions/kindr_tf.h>
+#include <minkindr_conversions/kindr_xml.h>
+#include <pcl/conversions.h>
+#include <pcl/filters/filter.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <std_srvs/Empty.h>
+#include <tf/transform_listener.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#include <voxblox/core/esdf_map.h>
+#include <voxblox/core/occupancy_map.h>
+#include <voxblox/core/tsdf_map.h>
+#include <voxblox/integrator/esdf_integrator.h>
+#include <voxblox/integrator/occupancy_integrator.h>
+#include <voxblox/integrator/tsdf_integrator.h>
+#include <voxblox/io/layer_io.h>
+#include <voxblox/io/mesh_ply.h>
+#include <voxblox/mesh/mesh_integrator.h>
+
+#include "voxblox_ros/mesh_vis.h"
+#include "voxblox_ros/ptcloud_vis.h"
+
+// This binary evaluates a pre-built voxblox map against a provided ground
+// truth dataset, provided as pointcloud, and outputs a variety of statistics.
+namespace voxblox {
+
+class VoxbloxEvaluator {
+ public:
+  VoxbloxEvaluator(const ros::NodeHandle& nh,
+                   const ros::NodeHandle& nh_private);
+  void evaluate();
+  void visualize();
+  bool shouldExit() const { return !visualize_; }
+
+ private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_private_;
+
+  // Whether to do the visualizations (involves generating mesh of the TSDF
+  // layer) and keep alive (for visualization) after finishing the eval.
+  // Otherwise only outputs the evaluations statistics.
+  bool visualize_;
+  // Whether to recolor the voxels by error to the GT before generating a mesh.
+  bool recolor_by_error_;
+  // How to color the mesh.
+  ColorMode color_mode_;
+
+  // Transformation between the ground truth dataset and the voxblox map.
+  // The GT is transformed INTO the voxblox coordinate frame.
+  Transformation T_V_G_;
+
+  // Visualization publishers.
+  ros::Publisher mesh_pub_;
+  ros::Publisher gt_ptcloud_pub_;
+
+  // Core data to compare.
+  std::shared_ptr<Layer<TsdfVoxel> > tsdf_layer_;
+  pcl::PointCloud<pcl::PointXYZ> gt_ptcloud_;
+
+  // Mesh visualization.
+  std::shared_ptr<MeshLayer> mesh_layer_;
+  std::shared_ptr<MeshIntegrator> mesh_integrator_;
+};
+
+VoxbloxEvaluator::VoxbloxEvaluator(const ros::NodeHandle& nh,
+                                   const ros::NodeHandle& nh_private)
+    : nh_(nh),
+      nh_private_(nh_private),
+      visualize_(true),
+      recolor_by_error_(false) {
+  // Load parameters.
+  nh_private_.param("visualize", visualize_, visualize_);
+  nh_private_.param("recolor_by_error", recolor_by_error_, recolor_by_error_);
+
+  // Load transformations.
+  XmlRpc::XmlRpcValue T_V_G_xml;
+  if (nh_private_.getParam("T_V_G", T_V_G_xml)) {
+    kindr::minimal::xmlRpcToKindr(T_V_G_xml, &T_V_G_);
+    bool invert_static_tranform = false;
+    nh_private_.param("invert_T_V_G", invert_static_tranform,
+                      invert_static_tranform);
+    if (invert_static_tranform) {
+      T_V_G_ = T_V_G_.inverse();
+    }
+  }
+
+  // Load the actual map and GT.
+  // Just exit if there's any issues here (this is just an evaluation node,
+  // after all).
+  std::string voxblox_file_path, gt_file_path;
+  CHECK(nh_private_.getParam("voxblox_file_path", voxblox_file_path))
+      << "No file path provided for voxblox map! Set the \"voxblox_file_path\" "
+         "param.";
+  CHECK(nh_private_.getParam("gt_file_path", gt_file_path))
+      << "No file path provided for ground truth pointcloud! Set the "
+         "\"gt_file_path\" param.";
+
+  CHECK(io::LoadLayer<TsdfVoxel>(voxblox_file_path, &tsdf_layer_))
+      << "Could not load voxblox map.";
+  pcl::PLYReader ply_reader;
+
+  CHECK_EQ(ply_reader.read(gt_file_path, gt_ptcloud_), 0)
+      << "Could not load pointcloud ground truth.";
+
+  // If doing visualizations, initialize the publishers.
+  if (visualize_) {
+    mesh_pub_ =
+        nh_private_.advertise<visualization_msgs::MarkerArray>("mesh", 1, true);
+    gt_ptcloud_pub_ = nh_private_.advertise<pcl::PointCloud<pcl::PointXYZRGB> >(
+        "gt_ptcloud", 1, true);
+
+    std::string color_mode("color");
+    nh_private_.param("color_mode", color_mode, color_mode);
+    if (color_mode == "color") {
+      color_mode_ = ColorMode::kColor;
+    } else if (color_mode == "height") {
+      color_mode_ = ColorMode::kHeight;
+    } else if (color_mode == "normals") {
+      color_mode_ = ColorMode::kNormals;
+    } else if (color_mode == "lambert") {
+      color_mode_ = ColorMode::kLambert;
+    } else {  // Default case is gray.
+      color_mode_ = ColorMode::kGray;
+    }
+  }
+}
+
+void VoxbloxEvaluator::evaluate() {
+  if (visualize_) {
+    visualize();
+  }
+}
+
+void VoxbloxEvaluator::visualize() {
+  // Generate the mesh.
+  MeshIntegrator::Config mesh_config;
+  mesh_layer_.reset(new MeshLayer(tsdf_layer_->block_size()));
+  mesh_integrator_.reset(
+      new MeshIntegrator(mesh_config, tsdf_layer_.get(), mesh_layer_.get()));
+  mesh_integrator_->generateWholeMesh();
+
+  // Publish mesh.
+  visualization_msgs::MarkerArray marker_array;
+  marker_array.markers.resize(1);
+  fillMarkerWithMesh(mesh_layer_, color_mode_, &marker_array.markers[0]);
+  mesh_pub_.publish(marker_array);
+
+  gt_ptcloud_pub_.publish(gt_ptcloud_);
+}
+
+}  // namespace voxblox
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "voxblox_node");
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, false);
+  google::InstallFailureSignalHandler();
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_private("~");
+
+  voxblox::VoxbloxEvaluator eval(nh, nh_private);
+
+  ros::spinOnce();
+
+  eval.evaluate();
+
+  if (!eval.shouldExit()) {
+    ros::spin();
+  }
+  return 0;
+}
