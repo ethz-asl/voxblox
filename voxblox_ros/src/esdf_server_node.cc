52dad37ac590161f85de3ef7d6e32c9170162452
Helen Oleynikova <helenoleynikova@gmail.com>
2016-12-12 21:03:46 +0100

    Added server nodes.

---
+++ voxblox_ros/src/esdf_server_node.cc
@@ -0,0 +1,15 @@
+#include "voxblox_ros/esdf_server.h"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "voxblox");
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, false);
+  google::InstallFailureSignalHandler();
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_private("~");
+
+  voxblox::EsdfServer node(nh, nh_private);
+
+  ros::spin();
+  return 0;
+}
