fac978afea9f49556d780c95c34dec8b00ae6daa
Helen Oleynikova <helenoleynikova@gmail.com>
2016-12-12 19:13:18 +0100

    Add first pass at transformer and tsdf server.

---
+++ voxblox_ros/include/voxblox_ros/transformer.h
@@ -0,0 +1,59 @@
+#ifndef VOXBLOX_ROS_TRANSFORMER_H_
+#define VOXBLOX_ROS_TRANSFORMER_H_
+
+#include <deque>
+#include <geometry_msgs/TransformStamped.h>
+#include <string>
+#include <tf/transform_listener.h>
+
+#include <voxblox/core/common.h>
+
+namespace voxblox {
+
+// Class that binds to either the TF tree or resolves transformations from the
+// ROS parameter server, depending on settings loaded from ROS params.
+class Transformer {
+ public:
+  Transformer(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
+
+  bool lookupTransform(const std::string& from_frame,
+                       const std::string& to_frame, const ros::Time& timestamp,
+                       Transformation* transform);
+
+  void transformCallback(const geometry_msgs::TransformStamped& transform_msg);
+
+ private:
+  bool lookupTransformTf(const std::string& from_frame,
+                         const std::string& to_frame,
+                         const ros::Time& timestamp, Transformation* transform);
+
+  bool lookupTransformQueue(const std::string& from_frame,
+                            const std::string& to_frame,
+                            const ros::Time& timestamp,
+                            Transformation* transform);
+
+  // Global/map coordinate frame. Will always look up TF transforms to this
+  // frame.
+  std::string world_frame_;
+  // If set, overwrite sensor frame with this value. If empty, unused.
+  std::string sensor_frame_;
+  // Whether to use TF transform resolution (true) or fixed transforms from
+  // parameters and transform topics (false).
+  bool use_tf_transforms_;
+  int64_t timestamp_tolerance_ns_;
+  // B is the body frame of the robot, C is the camera/sensor frame creating
+  // the pointclouds, and D is the 'dynamic' frame; i.e., incoming messages
+  // are assumed to be T_G_D.
+  Transformation T_B_C_;
+  Transformation T_B_D_;
+
+  // Only used if use_tf_transforms_ set to false.
+  ros::Subscriber transform_sub_;
+
+  // Transform queue, used only when use_tf_transforms is false.
+  std::deque<geometry_msgs::TransformStamped> transform_queue_;
+};
+
+}  // namespace voxblox
+
+#endif  // VOXBLOX_ROS_TSDF_SERVER_H_
