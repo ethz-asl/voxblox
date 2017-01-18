ee8d13d429480a5486c918204d8052fce510ad9a
Helen Oleynikova <helenoleynikova@gmail.com>
2017-01-18 14:47:23 +0100

    Fix message definitions.

--- voxblox_ros/include/voxblox_ros/conversions.h
+++ voxblox_ros/include/voxblox_ros/conversions.h
@@ -27,6 +27,29 @@ inline void colorMsgToVoxblox(const std_msgs::ColorRGBA& color_msg,
   color->a = static_cast<uint8_t>(color_msg.a * 255.0);
 }
 
+// Declarations
+template <typename VoxelType>
+void serializeLayerAsMsg(const Layer<VoxelType>& layer,
+                         voxblox_msgs::Layer* msg);
+
+template <typename VoxelType>
+void deserializeMsgToLayer(const voxblox_msgs::Layer& msg,
+                           Layer<VoxelType>* layer);
+
+// Definitions, maybe move to impl file.
+template <typename VoxelType>
+void serializeLayerAsMsg(const Layer<VoxelType>& layer, bool only_updated,
+                         voxblox_msgs::Layer* msg) {
+  msg->voxels_per_side = layer.voxels_per_side();
+  msg->voxel_size = layer.voxel_size();
+
+  msg->layer_type = getVoxelType<VoxelType>();
+}
+
+template <typename VoxelType>
+void deserializeMsgToLayer(const voxblox_msgs::Layer& msg,
+                           Layer<VoxelType>* layer) {}
+
 }  // namespace voxblox
 
 #endif  // VOXBLOX_ROS_VISUALIZATION_H_
