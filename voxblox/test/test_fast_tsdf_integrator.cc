385c2f3a7ae26883bc541d6b7c250a7212f66611
Marcin <marcin.dymczyk@mavt.ethz.ch>
2017-05-04 21:09:02 +0200

    debugging integrator test

--- voxblox/test/test_fast_tsdf_integrator.cc
+++ voxblox/test/test_fast_tsdf_integrator.cc
@@ -17,6 +17,7 @@ class FastTsdfIntegratorTest : public ::testing::Test {
     fast_layer_(kVoxelSize, kVoxelsPerSide),
     baseline_integrator_(config_, &baseline_layer_),
     fast_integrator_(fast_config_, &fast_layer_) {
+        std::cout << "ctor" << std::endl;
   }
   virtual void SetUp() {
     std::default_random_engine gen(kSeed);
@@ -24,6 +25,7 @@ class FastTsdfIntegratorTest : public ::testing::Test {
     std::uniform_real_distribution<double> xy_dist(-1.0, 1.0);
     std::normal_distribution<double> z_dist(3.0, 0.5);
 
+    std::cout << "setup" << std::endl;
     for (size_t i = 0u; i < kNumCamerasToGenerate; ++i) {
         Transformation T_G_C;
         T_G_C_.push_back(T_G_C);
@@ -42,10 +44,11 @@ class FastTsdfIntegratorTest : public ::testing::Test {
         points_C_.push_back(pointcloud);
         colors_.push_back(colors);
     }
+    std::cout << "setup done" << std::endl;
   }
 
-  static constexpr size_t kNumCamerasToGenerate = 50u;
-  static constexpr size_t kNumPointsToGenerate = 2000u;
+  static constexpr size_t kNumCamerasToGenerate = 10u;
+  static constexpr size_t kNumPointsToGenerate = 200u;
   static constexpr size_t kSeed = 242u;
 
   static constexpr double kVoxelSize = 0.01;
@@ -62,16 +65,27 @@ class FastTsdfIntegratorTest : public ::testing::Test {
 
   voxblox::test::LayerTest<TsdfVoxel> layer_test_;
 
-  std::vector<Transformation> T_G_C_;
-  std::vector<Pointcloud> points_C_;
+  std::vector<Transformation, Eigen::aligned_allocator<Transformation>> T_G_C_;
+  std::vector<Pointcloud, Eigen::aligned_allocator<Pointcloud>> points_C_;
   std::vector<Colors> colors_;
+
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
 
 TEST_F(FastTsdfIntegratorTest, CompareIntegratorWithBaseline) {
+    std::cout << "test" << std::endl;
     for (size_t i = 0u; i < kNumCamerasToGenerate; ++i) {
+      std::cout << i << std::endl;
+      CHECK_LT(i, T_G_C_.size());
+      CHECK_LT(i, points_C_.size());
+      CHECK_LT(i, colors_.size());
       baseline_integrator_.integratePointCloud(T_G_C_[i], points_C_[i], colors_[i]);
+      std::cout << i << std::endl;
       fast_integrator_.integratePointCloud(T_G_C_[i], points_C_[i], colors_[i]);
+      std::cout << i << std::endl;
     }
+    std::cout << "v" << std::endl;
     layer_test_.CompareLayers(baseline_layer_, fast_layer_);
 }
 
