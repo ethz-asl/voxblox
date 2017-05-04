711f5d96ca9cd907a0870efa5ea45d7a7eab5925
Marius Fehr <marius.b.fehr@gmail.com>
2017-05-04 19:43:45 +0200

    add basic benchmark files

---
+++ voxblox/test/benchmark_e2e.cc
@@ -0,0 +1,26 @@
+#include <benchmark/benchmark.h>
+
+#include "voxblox/core/tsdf_map.h"
+#include "voxblox/integrator/tsdf_integrator.h"
+
+using namespace voxblox;  // NOLINT
+
+class E2EBenchmark : public ::benchmark::Fixture {
+ public:
+  void SetUp(const ::benchmark::State& st) {
+    // TODO(mfehr): IMPLEMENT
+  }
+
+  void TearDown(const ::benchmark::State&) {
+    // TODO(mfehr): IMPLEMENT
+  }
+};
+
+BENCHMARK_DEFINE_F(E2EBenchmark, BM_TODO)(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    // TODO(mfehr): IMPLEMENT
+  }
+}
+BENCHMARK_REGISTER_F(E2EBenchmark, BM_TODO);
+
+BENCHMARK_MAIN();
