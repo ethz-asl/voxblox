e4959e8809b142c9b8844af53ef764bbd84f9363
Gene Merewether <mereweth@jpl.nasa.gov>
2017-06-03 21:58:45 -0700

    converting Tango TSDF to ESDF and serializing

--- voxblox/test/tango_tsdf_to_esdf.cc
+++ voxblox/test/tango_tsdf_to_esdf.cc
@@ -12,7 +12,7 @@
 #include <voxblox/mesh/mesh_integrator.h>
 
 #include <voxblox/core/esdf_map.h>
-#include <voxblox/integrator/esdf_integrator.h>
+#include <voxblox/integrator/esdf_tango_integrator.h>
 
 using namespace voxblox;  // NOLINT
 
@@ -33,20 +33,22 @@ int main(int argc, char** argv) {
   // ESDF maps.
   EsdfMap::Config esdf_config;
   std::shared_ptr<EsdfMap> esdf_map_;
-  std::unique_ptr<EsdfIntegrator> esdf_integrator_;
+  std::unique_ptr<EsdfTangoIntegrator> esdf_integrator_;
 
   // Same number of voxels per side for ESDF as with TSDF
   esdf_config.esdf_voxels_per_side = layer_from_file->voxels_per_side();
   esdf_map_.reset(new EsdfMap(esdf_config));
-  EsdfIntegrator::Config esdf_integrator_config;
+  EsdfTangoIntegrator::Config esdf_integrator_config;
   // Make sure that this is the same as the truncation distance OR SMALLER!
   esdf_integrator_config.min_distance_m = esdf_config.esdf_voxel_size;
   // esdf_integrator_config.min_distance_m =
   //    tsdf_integrator_->getConfig().default_truncation_distance;
-  esdf_integrator_.reset(new EsdfIntegrator(esdf_integrator_config,
-                                            layer_from_file.get(),
-                                            esdf_map_->getEsdfLayerPtr()));
+  esdf_integrator_.reset(new EsdfTangoIntegrator(esdf_integrator_config,
+                                                 layer_from_file.get(),
+                                                 esdf_map_->getEsdfLayerPtr()));
   esdf_integrator_->updateFromTsdfLayerBatch();
+
+  io::SaveLayer(esdf_map_->getEsdfLayer(), argv[2]);
 
   return 0;
 }
