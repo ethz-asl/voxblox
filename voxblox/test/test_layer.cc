f2c6176719374f65f7dd3d60e0dca04fb1a7f2fa
Marius Fehr <marius.b.fehr@gmail.com>
2017-07-22 19:20:43 +0200

    add deep copy ctor and unit tests, extend unit testing of serialization (esdf/occupancy)

---
+++ voxblox/test/test_layer.cc
@@ -0,0 +1,62 @@
+#include <iostream>  // NOLINT
+
+#include <gtest/gtest.h>
+
+#include "./Block.pb.h"
+#include "./Layer.pb.h"
+#include "voxblox/core/block.h"
+#include "voxblox/core/layer.h"
+#include "voxblox/core/voxel.h"
+#include "voxblox/io/layer_io.h"
+#include "voxblox/test/layer_test_utils.h"
+
+using namespace voxblox;  // NOLINT
+
+template <typename VoxelType>
+class LayerTest : public ::testing::Test,
+                  public voxblox::test::LayerTest<VoxelType> {
+ protected:
+  virtual void SetUp() {
+    layer_.reset(new Layer<VoxelType>(voxel_size_, voxels_per_side_));
+    SetUpLayer();
+  }
+
+  void SetUpLayer() const {
+    voxblox::test::SetUpTestLayer(kBlockVolumeDiameter, layer_.get());
+  }
+
+  typename Layer<VoxelType>::Ptr layer_;
+
+  const double voxel_size_ = 0.02;
+  const size_t voxels_per_side_ = 16u;
+
+  static constexpr size_t kBlockVolumeDiameter = 10u;
+};
+
+typedef LayerTest<TsdfVoxel> TsdfLayerTest;
+typedef LayerTest<EsdfVoxel> EsdfLayerTest;
+typedef LayerTest<OccupancyVoxel> OccupancyLayerTest;
+
+TEST_F(TsdfLayerTest, DeepCopyConstructor) {
+  Layer<TsdfVoxel> new_layer(*layer_);
+  EXPECT_TRUE(voxblox::test::IsSameLayer(new_layer, *layer_));
+}
+
+TEST_F(EsdfLayerTest, DeepCopyConstructor) {
+  Layer<EsdfVoxel> new_layer(*layer_);
+  EXPECT_TRUE(voxblox::test::IsSameLayer(new_layer, *layer_));
+}
+
+TEST_F(OccupancyLayerTest, DeepCopyConstructor) {
+  Layer<OccupancyVoxel> new_layer(*layer_);
+  EXPECT_TRUE(voxblox::test::IsSameLayer(new_layer, *layer_));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
+
+  int result = RUN_ALL_TESTS();
+
+  return result;
+}
