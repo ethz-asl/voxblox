7c1329ae643c99f1eb7bb455c653b470236ef750
Fabian Blöchliger <f.bloechliger@gmail.com>
2017-08-04 17:50:08 +0200

    Change from DCHECK to CHECK.

--- voxblox/include/voxblox/integrator/integrator_utils.h
+++ voxblox/include/voxblox/integrator/integrator_utils.h
@@ -63,7 +63,7 @@ class RayCaster {
   }
 
   bool nextRayIndex(AnyIndex* ray_index) {
-    DCHECK_NOTNULL(ray_index);
+    CHECK_NOTNULL(ray_index);
     *ray_index = curr_index_;
     bool not_at_end = curr_index_ != end_index_;
 
