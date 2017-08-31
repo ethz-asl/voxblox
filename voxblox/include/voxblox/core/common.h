634907e62c0997442180b98507140c979d7fae8c
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2017-08-31 17:58:19 +0200

    switched to floats

--- voxblox/include/voxblox/core/common.h
+++ voxblox/include/voxblox/core/common.h
@@ -38,7 +38,7 @@ inline std::shared_ptr<Type> aligned_shared(Arguments&&... arguments) {
 }
 
 // Types.
-typedef double FloatingPoint;
+typedef float FloatingPoint;
 typedef int IndexElement;
 
 typedef Eigen::Matrix<FloatingPoint, 3, 1> Point;
