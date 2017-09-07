023aa7513d9c0701b27c165f7926c58ce6120b02
Marius Fehr <marius.b.fehr@gmail.com>
2017-09-07 14:14:18 +0200

    decrease verbosity of integrator

--- voxblox/src/integrator/tsdf_integrator.cc
+++ voxblox/src/integrator/tsdf_integrator.cc
@@ -329,9 +329,9 @@ inline void MergedTsdfIntegrator::bundleRays(
     }
   }
 
-  LOG(INFO) << "Went from " << points_C.size() << " points to "
-            << voxel_map->size() << " raycasts  and " << clear_map->size()
-            << " clear rays.";
+  VLOG(3) << "Went from " << points_C.size() << " points to "
+          << voxel_map->size() << " raycasts  and " << clear_map->size()
+          << " clear rays.";
 }
 
 void MergedTsdfIntegrator::integrateVoxel(
