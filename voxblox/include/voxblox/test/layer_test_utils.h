a4917f6d00381a862030063aab9ba4a92c79ccc0
Marius Fehr <marius.b.fehr@gmail.com>
2017-11-22 21:27:28 +0100

    fix build

--- voxblox/include/voxblox/test/layer_test_utils.h
+++ voxblox/include/voxblox/test/layer_test_utils.h
@@ -163,13 +163,38 @@ void SetUpTestLayer(const IndexElement block_volume_diameter,
 }
 
 template <>
-void fillVoxelWithTestData(size_t x, size_t y, size_t z, TsdfVoxel* voxel);
+inline void fillVoxelWithTestData(size_t x, size_t y, size_t z,
+                                  TsdfVoxel* voxel) {
+  CHECK_NOTNULL(voxel);
+  voxel->distance = x * y * 0.66 + z;
+  voxel->weight = y * z * 0.33 + x;
+  voxel->color.r = static_cast<uint8_t>(x % 255);
+  voxel->color.g = static_cast<uint8_t>(y % 255);
+  voxel->color.b = static_cast<uint8_t>(z % 255);
+  voxel->color.a = static_cast<uint8_t>(x + y % 255);
+}
 
 template <>
-void fillVoxelWithTestData(size_t x, size_t y, size_t z, EsdfVoxel* voxel);
+inline void fillVoxelWithTestData(size_t x, size_t y, size_t z,
+                                  EsdfVoxel* voxel) {
+  CHECK_NOTNULL(voxel);
+  voxel->distance = x * y * 0.66 + z;
+  voxel->parent.x() = x % 255;
+  voxel->parent.y() = y % 255;
+  voxel->parent.z() = z % 255;
+
+  voxel->observed = true;
+  voxel->in_queue = true;
+  voxel->fixed = true;
+}
 
 template <>
-void fillVoxelWithTestData(size_t x, size_t y, size_t z, OccupancyVoxel* voxel);
+inline void fillVoxelWithTestData(size_t x, size_t y, size_t z,
+                                  OccupancyVoxel* voxel) {
+  CHECK_NOTNULL(voxel);
+  voxel->probability_log = x * y * 0.66 + z;
+  voxel->observed = true;
+}
 
 }  // namespace test
 }  // namespace voxblox
