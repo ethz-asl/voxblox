ea05ec62bd99197cf867fa209cedded33225fe91
Christian Witting <witting.chr@gmail.com>
2017-11-30 23:30:31 +0100

    Fixed compiler flags and initial warnings

--- voxblox/src/integrator/esdf_occ_integrator.cc
+++ voxblox/src/integrator/esdf_occ_integrator.cc
@@ -273,8 +273,8 @@ void EsdfOccIntegrator::getNeighbor(const BlockIndex& block_index,
                                     const Eigen::Vector3i& direction,
                                     BlockIndex* neighbor_block_index,
                                     VoxelIndex* neighbor_voxel_index) const {
-  DCHECK_NOTNULL(neighbor_block_index);
-  DCHECK_NOTNULL(neighbor_voxel_index);
+  DCHECK(neighbor_block_index != NULL);
+  DCHECK(neighbor_voxel_index != NULL);
 
   *neighbor_block_index = block_index;
   *neighbor_voxel_index = voxel_index + direction;
