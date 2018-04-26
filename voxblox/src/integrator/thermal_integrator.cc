dfcc2fd1926882ffb1ab7a3aecd94fd35806a954
Helen Oleynikova <helenoleynikova@gmail.com>
2018-04-26 15:54:55 +0200

    Have some key features of the integrator working.

--- voxblox/src/integrator/thermal_integrator.cc
+++ voxblox/src/integrator/thermal_integrator.cc
@@ -1,5 +1,7 @@
 #include "voxblox/integrator/thermal_integrator.h"
 
+#include "voxblox/utils/distance_utils.h"
+
 namespace voxblox {
 
 ThermalIntegrator::ThermalIntegrator(const Layer<TsdfVoxel>& tsdf_layer,
@@ -11,20 +13,33 @@ void ThermalIntegrator::addThermalBearingVectors(
     const std::vector<float>& temperatures) {
   CHECK_EQ(bearing_vectors.size(), temperatures.size())
       << "Temperature and bearing vector size does not match!";
+  constexpr FloatingPoint max_distance = 10.0;
+  const FloatingPoint temperature_prop_distance =
+      4 * tsdf_layer_.voxel_size();  // This equates to 2 * voxel_size
+                                      // truncation distance.
 
   for (size_t i = 0; i < bearing_vectors.size(); ++i) {
+    Point surface_intersection = Point::Zero();
     // Cast ray from the origin in the direction of the bearing vector until
     // finding an intersection with a surface.
+    bool success = getSurfaceDistanceAlongRay<TsdfVoxel>(
+        tsdf_layer_, origin, bearing_vectors[i], max_distance,
+        &surface_intersection);
 
+    if (!success) {
+      continue;
+    }
 
-
-
-
-
+    // Now look up the matching voxels in the thermal layer and mark them.
+    // Let's just start with 1.
+    Block<ThermalVoxel>::Ptr block_ptr =
+        thermal_layer_->allocateBlockPtrByCoordinates(surface_intersection);
+    ThermalVoxel& voxel =
+        block_ptr->getVoxelByCoordinates(surface_intersection);
+    voxel.temperature =
+        (voxel.observations * voxel.temperature + temperatures[i]) /
+        (++voxel.observations);
   }
-
-
-
 }
 
 }  // namespace voxblox
