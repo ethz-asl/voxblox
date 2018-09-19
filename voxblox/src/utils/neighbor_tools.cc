64450adb1c2ce97dbc3b36a3c7721b3c17b9f481
Marius Fehr <marius.b.fehr@gmail.com>
2018-09-19 12:54:35 +0200

    nit

--- voxblox/src/utils/neighbor_tools.cc
+++ voxblox/src/utils/neighbor_tools.cc
@@ -6,15 +6,17 @@ namespace voxblox {
 
 // clang-format off
 const NeighborhoodLookupTables::Distances NeighborhoodLookupTables::kDistances = [] {
+  constexpr float sqrt_2 = std::sqrt(2);
+  constexpr float sqrt_3 = std::sqrt(3);
   Distances distance_matrix;
   distance_matrix <<  1.f, 1.f, 1.f, 1.f, 1.f, 1.f,
 
-                      std::sqrt(2), std::sqrt(2), std::sqrt(2), std::sqrt(2),
-                      std::sqrt(2), std::sqrt(2), std::sqrt(2), std::sqrt(2),
-                      std::sqrt(2), std::sqrt(2), std::sqrt(2), std::sqrt(2),
+                      sqrt_2, sqrt_2, sqrt_2, sqrt_2,
+                      sqrt_2, sqrt_2, sqrt_2, sqrt_2,
+                      sqrt_2, sqrt_2, sqrt_2, sqrt_2,
 
-                      std::sqrt(3), std::sqrt(3), std::sqrt(3), std::sqrt(3),
-                      std::sqrt(3), std::sqrt(3), std::sqrt(3), std::sqrt(3);
+                      sqrt_3, sqrt_3, sqrt_3, sqrt_3,
+                      sqrt_3, sqrt_3, sqrt_3, sqrt_3;
   return distance_matrix;
 }();
 
