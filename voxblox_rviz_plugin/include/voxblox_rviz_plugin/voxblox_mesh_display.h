ee7043fc90a4a5dcabf8183d20fc19b85312725d
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-10-18 18:18:10 +0200

    added compressed mesh sending

--- voxblox_rviz_plugin/include/voxblox_rviz_plugin/voxblox_mesh_display.h
+++ voxblox_rviz_plugin/include/voxblox_rviz_plugin/voxblox_mesh_display.h
@@ -1,8 +1,12 @@
-#ifndef VOXBLOX_RVIZ_PLUGIN_VOXBLOX_MESH_DISPLAY_H
-#define VOXBLOX_RVIZ_PLUGIN_VOXBLOX_MESH_DISPLAY_H
+#ifndef VOXBLOX_RVIZ_PLUGIN_VOXBLOX_MESH_DISPLAY_H_
+#define VOXBLOX_RVIZ_PLUGIN_VOXBLOX_MESH_DISPLAY_H_
 
 #include <rviz/message_filter_display.h>
 #include <voxblox_msgs/Mesh.h>
+#include <voxblox_msgs/MinimalMesh.h>
+#include <memory>
+
+#include "voxblox_rviz_plugin/voxblox_mesh_visual.h"
 
 namespace voxblox_rviz_plugin {
 
@@ -12,6 +16,7 @@ class VoxbloxMeshDisplay
     : public rviz::MessageFilterDisplay<voxblox_msgs::Mesh> {
   Q_OBJECT
  public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   VoxbloxMeshDisplay();
   virtual ~VoxbloxMeshDisplay();
 
@@ -26,6 +31,26 @@ class VoxbloxMeshDisplay
   std::unique_ptr<VoxbloxMeshVisual> visual_;
 };
 
+// lots of copy pasted code, but the Q_OBJECT macro doesn't support templates
+class VoxbloxMinimalMeshDisplay
+    : public rviz::MessageFilterDisplay<voxblox_msgs::MinimalMesh> {
+  Q_OBJECT
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  VoxbloxMinimalMeshDisplay();
+  virtual ~VoxbloxMinimalMeshDisplay();
+
+ protected:
+  virtual void onInitialize();
+
+  virtual void reset();
+
+ private:
+  void processMessage(const voxblox_msgs::MinimalMesh::ConstPtr& msg);
+
+  std::unique_ptr<VoxbloxMinimalMeshVisual> visual_;
+};
+
 }  // namespace voxblox_rviz_plugin
 
-#endif  // VOXBLOX_RVIZ_PLUGIN_VOXBLOX_MESH_DISPLAY_H
+#endif  // VOXBLOX_RVIZ_PLUGIN_VOXBLOX_MESH_DISPLAY_H_
--- voxblox_rviz_plugin/src/voxblox_mesh_visual.cc
+++ voxblox_rviz_plugin/src/voxblox_mesh_visual.cc
@@ -31,9 +31,8 @@ void VoxbloxMeshVisual::setMessage(const voxblox_msgs::Mesh::ConstPtr& msg) {
     const voxblox::AnyIndexHashMapType<
         Ogre::ManualObject*>::type::const_iterator it = object_map_.find(index);
     if (it != object_map_.end()) {
-
-      //delete empty mesh blocks
-      if(mesh_block.triangles.size() == 0){
+      // delete empty mesh blocks
+      if (mesh_block.triangles.size() == 0) {
         scene_manager_->destroyManualObject(it->second);
         object_map_.erase(it);
         continue;
@@ -84,6 +83,113 @@ void VoxbloxMeshVisual::setFramePosition(const Ogre::Vector3& position) {
 }
 
 void VoxbloxMeshVisual::setFrameOrientation(
+    const Ogre::Quaternion& orientation) {
+  frame_node_->setOrientation(orientation);
+}
+
+unsigned int VoxbloxMinimalMeshVisual::instance_counter_ = 0;
+
+VoxbloxMinimalMeshVisual::VoxbloxMinimalMeshVisual(
+    Ogre::SceneManager* scene_manager, Ogre::SceneNode* parent_node) {
+  scene_manager_ = scene_manager;
+  frame_node_ = parent_node->createChildSceneNode();
+  instance_number_ = instance_counter_++;
+}
+
+VoxbloxMinimalMeshVisual::~VoxbloxMinimalMeshVisual() {
+  // Destroy all the objects
+  for (std::pair<const voxblox::BlockIndex, Ogre::ManualObject*> ogre_object :
+       object_map_) {
+    scene_manager_->destroyManualObject(ogre_object.second);
+  }
+}
+
+void VoxbloxMinimalMeshVisual::setMessage(
+    const voxblox_msgs::MinimalMesh::ConstPtr& msg) {
+  for (const voxblox_msgs::MinimalMeshBlock& mesh_block : msg->mesh_blocks) {
+    const voxblox::BlockIndex index(mesh_block.index[0], mesh_block.index[1],
+                                    mesh_block.index[2]);
+
+    Ogre::ManualObject* ogre_object;
+    const voxblox::AnyIndexHashMapType<
+        Ogre::ManualObject*>::type::const_iterator it = object_map_.find(index);
+    if (it != object_map_.end()) {
+      // delete empty mesh blocks
+      if (mesh_block.triangles.size() == 0) {
+        scene_manager_->destroyManualObject(it->second);
+        object_map_.erase(it);
+        continue;
+      }
+
+      ogre_object = it->second;
+      ogre_object->clear();
+    } else {
+      std::string object_name = std::to_string(index.x()) + std::string(" ") +
+                                std::to_string(index.y()) + std::string(" ") +
+                                std::to_string(index.z()) + std::string(" ") +
+                                std::to_string(instance_number_);
+      ogre_object = scene_manager_->createManualObject(object_name);
+      object_map_.insert(std::make_pair(index, ogre_object));
+
+      frame_node_->attachObject(ogre_object);
+    }
+
+    DCHECK(ogre_object != nullptr);
+
+    ogre_object->estimateVertexCount(3 * mesh_block.triangles.size());
+    ogre_object->begin("BaseWhiteNoLighting",
+                       Ogre::RenderOperation::OT_TRIANGLE_LIST);
+
+    for (const voxblox_msgs::MinimalTriangle& triangle : mesh_block.triangles) {
+      Ogre::Vector3 points[3];
+
+      for (size_t i = 0; i < 3; ++i) {
+        constexpr float point_conv_factor = 2.0f / 65535.0f;
+        const float mesh_x =
+            (static_cast<float>(triangle.x[i]) * point_conv_factor +
+             static_cast<float>(index[0])) *
+            msg->block_edge_length;
+        const float mesh_y =
+            (static_cast<float>(triangle.y[i]) * point_conv_factor +
+             static_cast<float>(index[1])) *
+            msg->block_edge_length;
+        const float mesh_z =
+            (static_cast<float>(triangle.z[i]) * point_conv_factor +
+             static_cast<float>(index[2])) *
+            msg->block_edge_length;
+
+        points[i] = Ogre::Vector3(mesh_x, mesh_y, mesh_z);
+      }
+
+      // color by normals
+      const Ogre::Vector3 dir0 = points[0] - points[1];
+      const Ogre::Vector3 dir1 = points[0] - points[2];
+      const Ogre::Vector3 normal = dir0.crossProduct(dir1).normalisedCopy();
+
+      Ogre::ColourValue color;
+      color.r = normal.x * 0.5f + 0.5f;
+      color.g = normal.y * 0.5f + 0.5f;
+      color.b = normal.z * 0.5f + 0.5f;
+      color.a = 1.0f;
+
+      // add triangle to ogre
+      ogre_object->position(points[0]);
+      ogre_object->colour(color);
+      ogre_object->position(points[1]);
+      ogre_object->colour(color);
+      ogre_object->position(points[2]);
+      ogre_object->colour(color);
+    }
+
+    ogre_object->end();
+  }
+}
+
+void VoxbloxMinimalMeshVisual::setFramePosition(const Ogre::Vector3& position) {
+  frame_node_->setPosition(position);
+}
+
+void VoxbloxMinimalMeshVisual::setFrameOrientation(
     const Ogre::Quaternion& orientation) {
   frame_node_->setOrientation(orientation);
 }
