fb8de8741e5fa9099536d5db2918d13d1becbeb3
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-10-19 11:21:22 +0200

    starting to make changes to allow smooth rendering

--- voxblox_rviz_plugin/src/voxblox_mesh_visual.cc
+++ voxblox_rviz_plugin/src/voxblox_mesh_visual.cc
@@ -1,5 +1,6 @@
 #include <OGRE/OgreSceneManager.h>
 #include <OGRE/OgreSceneNode.h>
+#include <voxblox/mesh/mesh_utils.h>
 
 #include "voxblox_rviz_plugin/voxblox_mesh_visual.h"
 
@@ -106,10 +107,59 @@ VoxbloxMinimalMeshVisual::~VoxbloxMinimalMeshVisual() {
 
 void VoxbloxMinimalMeshVisual::setMessage(
     const voxblox_msgs::MinimalMesh::ConstPtr& msg) {
+  size_t vertex_index = 0u;
+
   for (const voxblox_msgs::MinimalMeshBlock& mesh_block : msg->mesh_blocks) {
     const voxblox::BlockIndex index(mesh_block.index[0], mesh_block.index[1],
                                     mesh_block.index[2]);
 
+    voxblox::Mesh mesh;
+    for (const voxblox_msgs::MinimalTriangle& triangle : mesh_block.triangles) {
+      for (size_t i = 0; i < 3; ++i) {
+        constexpr float point_conv_factor = 2.0f / 65535.0f;
+        const float mesh_x =
+            (static_cast<float>(triangle.x[i]) * point_conv_factor +
+             static_cast<float>(index[0])) *
+            msg->block_edge_length;
+        const float mesh_y =
+            (static_cast<float>(triangle.y[i]) * point_conv_factor +
+             static_cast<float>(index[1])) *
+            msg->block_edge_length;
+        const float mesh_z =
+            (static_cast<float>(triangle.z[i]) * point_conv_factor +
+             static_cast<float>(index[2])) *
+            msg->block_edge_length;
+
+        mesh.indices.push_back(vertex_index++);
+        mesh.vertices.emplace_back(mesh_x, mesh_y, mesh_z);
+      }
+
+      // calculate normals
+      const voxblox::Point dir0 =
+          mesh.vertices.rbegin()[2] - mesh.vertices.rbegin()[1];
+      const voxblox::Point dir1 =
+          mesh.vertices.rbegin()[2] - mesh.vertices.back();
+      const voxblox::Point normal = dir0.cross(dir1).normalized();
+
+      mesh.normals.push_back(normal);
+      mesh.normals.push_back(normal);
+      mesh.normals.push_back(normal);
+    }
+
+    voxblox::Mesh connected_mesh;
+    voxblox::createConnectedMesh(mesh, &connected_mesh);
+
+    // add color information
+    mesh.colors.reserve(mesh.normals.size());
+    for (const Point& normal : mesh.normals.size()) {
+      voxblox::Color color;
+      color.r = 255.0f * (normal.x() * 0.5 + 0.5);
+      color.g = 255.0f * (normal.x() * 0.5 + 0.5);
+      color.b = 255.0f * (normal.x() * 0.5 + 0.5);
+      color.a = 255;
+    }
+
+    // got mesh now create ogre object
     Ogre::ManualObject* ogre_object;
     const voxblox::AnyIndexHashMapType<
         Ogre::ManualObject*>::type::const_iterator it = object_map_.find(index);
@@ -136,54 +186,35 @@ void VoxbloxMinimalMeshVisual::setMessage(
 
     DCHECK(ogre_object != nullptr);
 
-    ogre_object->estimateVertexCount(3 * mesh_block.triangles.size());
-    ogre_object->begin("BaseWhiteNoLighting",
-                       Ogre::RenderOperation::OT_TRIANGLE_LIST);
+    ogre_object->estimateVertexCount(mesh.vertices.size());
+    ogre_object->estimateIndexCount(mesh.indicies.size());
+    ogre_object->begin("BaseWhite", Ogre::RenderOperation::OT_TRIANGLE_LIST);
 
-    for (const voxblox_msgs::MinimalTriangle& triangle : mesh_block.triangles) {
-      Ogre::Vector3 points[3];
+    for (size_t i = 0; i < mesh.vertices.size(); ++i) {
+      // note calling position changes what vertex the color and normal calls
+      // point to
+      ogre_object->position(mesh.vertices[i].x(), mesh.vertices[i].y(),
+                            mesh.vertices[i].z());
 
-      for (size_t i = 0; i < 3; ++i) {
-        constexpr float point_conv_factor = 2.0f / 65535.0f;
-        const float mesh_x =
-            (static_cast<float>(triangle.x[i]) * point_conv_factor +
-             static_cast<float>(index[0])) *
-            msg->block_edge_length;
-        const float mesh_y =
-            (static_cast<float>(triangle.y[i]) * point_conv_factor +
-             static_cast<float>(index[1])) *
-            msg->block_edge_length;
-        const float mesh_z =
-            (static_cast<float>(triangle.z[i]) * point_conv_factor +
-             static_cast<float>(index[2])) *
-            msg->block_edge_length;
+      ogre_object->normal(mesh.normals[i].x(), mesh.normals[i].y(),
+                          mesh.normals[i].z());
 
-        points[i] = Ogre::Vector3(mesh_x, mesh_y, mesh_z);
-      }
+      constexpr float color_conv_factor = 1.0f / 255.0f;
+      ogre_object->colour(
+          color_conv_factor * static_cast<float>(mesh.colors[i].r),
+          color_conv_factor * static_cast<float>(mesh.colors[i].g),
+          color_conv_factor * static_cast<float>(mesh.colors[i].b),
+          color_conv_factor * static_cast<float>(mesh.colors[i].a));
+    }
 
-      // color by normals
-      const Ogre::Vector3 dir0 = points[0] - points[1];
-      const Ogre::Vector3 dir1 = points[0] - points[2];
-      const Ogre::Vector3 normal = dir0.crossProduct(dir1).normalisedCopy();
-
-      Ogre::ColourValue color;
-      color.r = normal.x * 0.5f + 0.5f;
-      color.g = normal.y * 0.5f + 0.5f;
-      color.b = normal.z * 0.5f + 0.5f;
-      color.a = 1.0f;
-
-      // add triangle to ogre
-      ogre_object->position(points[0]);
-      ogre_object->colour(color);
-      ogre_object->position(points[1]);
-      ogre_object->colour(color);
-      ogre_object->position(points[2]);
-      ogre_object->colour(color);
+    // needed for anything other than flat rendering
+    for (const voxblox::VertexIndex index : mesh.indicies) {
+      ogre_object->index(index);
     }
 
     ogre_object->end();
   }
-}
+}  // namespace voxblox_rviz_plugin
 
 void VoxbloxMinimalMeshVisual::setFramePosition(const Ogre::Vector3& position) {
   frame_node_->setPosition(position);
