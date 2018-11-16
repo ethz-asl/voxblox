b403ca120c851b34d11814c6d3238621031e0d56
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-11-16 17:04:05 +0100

    cleaned up autogen files and got sphnix working

---
+++ voxblox/docs/conf.py
@@ -0,0 +1,37 @@
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# The `extensions` list should already be in here from `sphinx-quickstart`
+extensions = [
+    # there may be others here already, e.g. 'sphinx.ext.mathjax'
+    'breathe',
+    'exhale'
+]
+
+# Setup the breathe extension
+breathe_projects = {"voxblox": "./doxyoutput/xml"}
+breathe_default_project = "voxblox"
+
+# Setup the exhale extension
+exhale_args = {
+    # These arguments are required
+    "verboseBuild": True,
+    "containmentFolder": "./api",
+    "rootFileName": "library_root.rst",
+    "rootFileTitle": "Library API",
+    "doxygenStripFromPath": "..",
+    # Suggested optional arguments
+    "createTreeView": True,
+    # TIP: if using the sphinx-bootstrap-theme, you need
+    #"treeViewIsBootstrap": True,
+    "exhaleExecutesDoxygen": True,
+    "exhaleUseDoxyfile": True
+}
+
+# Tell sphinx what the primary language being documented is.
+primary_domain = 'cpp'
+
+# Tell sphinx what the pygments highlight language should be.
+highlight_language = 'cpp'
