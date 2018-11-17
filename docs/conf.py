74aabb850f2e9ec67105cd982eb652b348c444ed
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-11-17 17:48:10 +0100

    add exhale requirements

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,10 @@
-import sphinx_rtd_theme
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_theme_options = {
     'logo_only': True
 }
