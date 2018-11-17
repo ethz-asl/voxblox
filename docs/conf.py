ea3e7bc8366ddd119f1a6808c920904ba8a73f82
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-11-17 17:38:16 +0100

    gray fox

--- docs/conf.py
+++ docs/conf.py
@@ -2,10 +2,12 @@ import sphinx_rtd_theme
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    'logo_only': True
+}
+html_logo = "logo.gif"
 
-# The `extensions` list should already be in here from `sphinx-quickstart`
 extensions = [
-    # there may be others here already, e.g. 'sphinx.ext.mathjax'
     'breathe',
     'exhale',
     'sphinx.ext.autosectionlabel'
@@ -17,16 +19,12 @@ breathe_default_project = "voxblox"
 
 # Setup the exhale extension
 exhale_args = {
-    # These arguments are required
     "verboseBuild": False,
     "containmentFolder": "./api",
     "rootFileName": "library_root.rst",
     "rootFileTitle": "Library API",
     "doxygenStripFromPath": "..",
-    # Suggested optional arguments
     "createTreeView": True,
-    # TIP: if using the sphinx-bootstrap-theme, you need
-    #"treeViewIsBootstrap": True,
     "exhaleExecutesDoxygen": True,
     "exhaleUseDoxyfile": True
 }
