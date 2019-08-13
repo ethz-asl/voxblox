06977ea639cbcd8b864a93162866f10e414eecba
Laura <gasserl@student.ethz.ch>
2019-08-13 10:31:12 +0200

    const attribute get tsdf map from server

--- voxblox_ros/include/voxblox_ros/tsdf_server.h
+++ voxblox_ros/include/voxblox_ros/tsdf_server.h
@@ -95,7 +95,7 @@ class TsdfServer {
   void updateMeshEvent(const ros::TimerEvent& event);
   void publishMapEvent(const ros::TimerEvent& event);
 
-  std::shared_ptr<TsdfMap> getTsdfMapPtr() { return tsdf_map_; }
+  std::shared_ptr<TsdfMap> getTsdfMapPtr() const { return tsdf_map_; };
 
   /// Accessors for setting and getting parameters.
   double getSliceLevel() const { return slice_level_; }
